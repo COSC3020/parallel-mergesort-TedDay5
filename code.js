395b45844c0cfcbd4f7acac0b7dd67068f1357d3
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-06 20:09:24 -0700

    Setting up functions

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
+const async = require('async');
 
+async function parallelMergeSort(n, low, high, temp) {
+
+}
+
+function merge(n, low, middle, high, temp) {
+
+}
+
+async function mergeSort(n) {
+
+}
