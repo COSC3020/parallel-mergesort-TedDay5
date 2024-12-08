5297eae86f1bef6251dc90057860f026b314c9d7
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-08 03:11:01 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -2,3 +2,13 @@ const fs = require('fs');
 const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
+
+async function testParallelMergeSort() {
+    let array = [38, 27, 43, 3, 9, 82, 10];
+    let expected = [3, 9, 10, 27, 38, 43, 82];
+    await mergeSort(array);
+    assert.deepStrictEqual(array, expected, 'Not sorted correctly');
+    console.log('Test passed');
+}
+
+testParallelMergeSort();
