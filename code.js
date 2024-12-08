f039a3067269edfb394ada24e2e3b62c9331ac0d
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-08 03:08:00 -0700

    implemented parallelMergeSort merge and mergeSort functions

--- code.js
+++ code.js
@@ -1,13 +1,32 @@
 const async = require('async');
 
-async function parallelMergeSort(n, low, high, temp) {
+async function parallelMergeSort(array, low, high, temp) {
+    if(low >= high) {
+        return;
+    }
+    let middle = Math.floor((low + high) / 2);
+    const left = parallelMergeSort(array, low, middle, temp);
+    const right = parallelMergeSort(array, middle + 1, high, temp);
 
+    await Promise.all([left][right])
+    merge(array, low, middle, high, temp)
 }
 
-function merge(n, low, middle, high, temp) {
-
+function merge(array, low, middle, high, temp) {
+    for(let i = low; i <= high; i++) {
+        if(low <= middle && (middle + 1 > high || array[low] < array[middle + 1])) {
+            temp[i] = array[low++];
+        }
+        else {
+            temp[i] = array[(middle + 1)++];
+        }
+    }
+    for(let i = low; i <= high; i++) {
+        array[i] = temp[i];
+    }
 }
 
-async function mergeSort(n) {
-
+async function mergeSort(array) {
+    let temp = [];
+    sort = await parallelMergeSort(array, 0, array.length - 1, temp);
 }
